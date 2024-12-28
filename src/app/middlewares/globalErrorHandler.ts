d0ab396db2091bcb1ec53c4bbaf72cdcb76022b9
Md Sajedul Islam <65823258+mdsajedulra@users.noreply.github.com>
2024-12-28 10:33:23 +0600

    global error handler added

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from "express";
+import { handlerZodError } from "../helpers/handleZodError";
+import mongoose from "mongoose";
+import { handleCastError } from "../helpers/handleCastError";
+import { handleValidationError } from "../helpers/handleValidationError";
+import { handlerDuplicateError } from "../helpers/handleDuplicateError";
+import { handleGenericError } from "../helpers/handleGenericError";
+
+export const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err.name && err.name === "ZodError") {
+    handlerZodError(err, res);
+  } else if (err instanceof mongoose.Error.CastError) {
+    handleCastError(err, res);
+  } else if (err instanceof mongoose.Error.ValidationError) {
+    handleValidationError(err, res);
+  } else if (err.code && err.code === 11000) {
+    handlerDuplicateError(err, res);
+  } else if (err instanceof Error) {
+    handleGenericError(err, res);
+  }
+};
